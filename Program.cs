858836b5668f83e7894d93b39e79424ec1381810
ramirojhoel <ramirojhoel1998@gmail.com>
2021-10-29 00:25:18 -0400

    codigoprueba3

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace prueba3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("como sea 3!");
+        }
+    }
+}
